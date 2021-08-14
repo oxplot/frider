7e989cc39dd0d861aa8804e9c9479908c82e46b1
Mansour Behabadi <mansour@oxplot.com>
2021-08-14 17:09:39 +1000

    Calc uid

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/oxplot/frider
 go 1.16
 
 require (
-	github.com/gorilla/feeds v1.1.1
 	github.com/joho/godotenv v1.3.0
 	github.com/kr/pretty v0.3.0 // indirect
+	github.com/mmcdole/gofeed v1.1.3
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
--- main.go
+++ main.go
@@ -1,58 +1,122 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
 	"flag"
 	"fmt"
+	htemplate "html/template"
 	"log"
 	"net/url"
 	"os"
+	ttemplate "text/template"
+	"time"
 
-	_ "github.com/gorilla/feeds"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/mmcdole/gofeed"
 	"gopkg.in/yaml.v3"
 )
 
+const (
+	sameDomainRequestDelay = time.Second * 2
+)
+
 type Config struct {
-	Feeds []*FeedSpec `yaml:"feeds"`
-	SMTP  struct {
-		Sender     string   `yaml:"sender"`
+	Storage struct {
+		Path        string `yaml:"path"`
+		UID         string `yaml:"uid"`
+		uidTemplate *ttemplate.Template
+	} `yaml:"storage"`
+	SMTP struct {
+		Sender     string `yaml:"sender"`
+		senderTpl  *ttemplate.Template
 		Recipients []string `yaml:"recipients"`
 		Host       string   `yaml:"host"`
-		Port       string   `yaml:"port"`
+		Port       uint     `yaml:"port"`
 		Username   string   `yaml:"username"`
 		Password   string   `yaml:"password"`
+		Jobs       uint     `yaml:"jobs"`
 	} `yaml:"smtp"`
 	Email struct {
-		Subject     string `yaml:"subject"`
-		HTMLContent string `yaml:"html_content"`
+		Subject    string `yaml:"subject"`
+		subjectTpl *ttemplate.Template
+		Content    string `yaml:"content"`
+		contentTpl *htemplate.Template
 	} `yaml:"email"`
+	Feeds []*FeedSpec `yaml:"feeds"`
 }
 
 type FeedSpec struct {
-	Name      string   `yaml:"name"`
-	URL       string   `yaml:"url"`
-	parsedURL *url.URL `yaml:"-"`
+	Name      string `yaml:"name"`
+	URL       string `yaml:"url"`
+	parsedURL *url.URL
+}
+
+type feedItem struct {
+	Feed *gofeed.Feed
+	Item *gofeed.Item
 }
 
 var (
-	configPath = flag.String("config", os.Getenv("FRIDER_CONFIG"), "path to config file")
-	config     *Config
+	configPath  = flag.String("config", os.Getenv("FRIDER_CONFIG"), "path to config file")
+	printConfig = flag.Bool("print-config", false, "output current config to stdout and exit")
+	config      *Config
 )
 
+func calcItemUID(i feedItem) (string, error) {
+	var buf bytes.Buffer
+	if err := config.Storage.uidTemplate.Execute(&buf, i); err != nil {
+		return "", fmt.Errorf("cannot calculate UID")
+	}
+	return fmt.Sprintf("%x", sha256.Sum256(buf.Bytes())), nil
+}
+
 func processDomainFeeds(c chan *FeedSpec, done chan bool) {
-	for f := range c {
-		log.Printf("%#v", f)
+	parser := gofeed.NewParser()
+	for fs := range c {
+		time.Sleep(sameDomainRequestDelay)
+		f, err := parser.ParseURL(fs.URL)
+		if err != nil {
+			log.Printf("warn: failed to parse feed '%s' url '%s': %s", fs.Name, fs.URL, err)
+			continue
+		}
+		for _, i := range f.Items {
+			fi := feedItem{Feed: f, Item: i}
+			uid, err := calcItemUID(fi)
+			if err != nil {
+				log.Printf("warn: failed to calculate item uid in feed '%s': %s", fs.Name, err)
+				continue
+			}
+			log.Printf("uid: %s", uid)
+		}
 	}
 	close(done)
 }
 
-func processFeeds(c chan *FeedSpec, done chan bool) {
+func run() error {
+	var err error
+	if config, err = loadConfig(*configPath); err != nil {
+		return fmt.Errorf("failed to load config: %s", err)
+	}
+
+	if *printConfig {
+		return yaml.NewEncoder(os.Stdout).Encode(config)
+	}
+
 	type domainFeed struct {
 		c    chan *FeedSpec
 		done chan bool
 	}
 	domains := map[string]domainFeed{}
-	for f := range c {
+
+	for _, f := range config.Feeds {
+		u, err := url.Parse(f.URL)
+		if err != nil {
+			log.Printf("warn: cannot parse '%s' feed URL '%s': %s", f.Name, f.URL, err)
+			continue
+		}
+		f.parsedURL = u
+
 		dom, ok := domains[f.parsedURL.Host]
 		if !ok {
 			dom = domainFeed{c: make(chan *FeedSpec), done: make(chan bool)}
@@ -61,13 +125,24 @@ func processFeeds(c chan *FeedSpec, done chan bool) {
 		}
 		dom.c <- f
 	}
+
+	for _, d := range domains {
+		close(d.c)
+	}
 	for _, d := range domains {
 		<-d.done
 	}
-	close(done)
+
+	return nil
 }
 
 func loadConfig(path string) (*Config, error) {
+	var c Config
+	c.Storage.UID = "{{.Feed.Link}}|{{.Feed.FeedLink}}|{{.Item.Title}}|{{.Item.Link}}|{{.Item.GUID}}|{{.Item.Content}}"
+	c.SMTP.Jobs = 4
+	c.Email.Subject = "{{.Item.Title}}"
+	c.Email.Content = "<h2><a href=\"{{.Item.Link}}\">{{.Item.Title}}</a></h2>{{.Item.Html}}"
+
 	var f *os.File
 	var err error
 	if path == "-" {
@@ -80,33 +155,26 @@ func loadConfig(path string) (*Config, error) {
 	}
 	defer f.Close()
 	y := yaml.NewDecoder(f)
-	var c Config
 	if err = y.Decode(&c); err != nil {
 		return nil, err
 	}
+	c.Storage.uidTemplate, err = ttemplate.New("").Parse(c.Storage.UID)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse storage.uid '%s': %s", c.Storage.UID, err)
+	}
+	c.SMTP.senderTpl, err = ttemplate.New("").Parse(c.SMTP.Sender)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse smtp.sender '%s': %s", c.SMTP.Sender, err)
+	}
+	c.Email.subjectTpl, err = ttemplate.New("").Parse(c.Email.Subject)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse email.subject '%s': %s", c.Email.Subject, err)
+	}
+	c.Email.contentTpl, err = htemplate.New("").Parse(c.Email.Content)
+	if err != nil {
+		return nil, fmt.Errorf("can't parse email.content '%s': %s", c.Email.Content, err)
+	}
 	return &c, nil
-}
-
-func run() error {
-	var err error
-	if config, err = loadConfig(*configPath); err != nil {
-		return fmt.Errorf("failed to load config: %s", err)
-	}
-	c := make(chan *FeedSpec)
-	done := make(chan bool)
-	go processFeeds(c, done)
-	for name, f := range config.Feeds {
-		u, err := url.Parse(f.URL)
-		if err != nil {
-			log.Printf("warn: cannot parse '%s' feed URL '%s': %s", f.name, f.URL, err)
-			continue
-		}
-		f.parsedURL = u
-		f.name = name
-		c <- f
-	}
-	<-done
-	return nil
 }
 
 func main() {
